5ba8ef04d34dc2f7bca8731da3c6f6403132cd1b
Kirill <kirill_kornew@mail.ru>
2020-06-10 15:24:08 +0300

    Initial Commit

---
+++ Quick search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick search
+//
+//  Created by Кирилл on 6/10/20.
+//  Copyright © 2020 Kornev.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
