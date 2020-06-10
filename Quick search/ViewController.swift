92e0440383c72e2df196438b792849980acb70d1
Kirill <kirill_kornew@mail.ru>
2020-06-10 15:37:46 +0300

    added code icons launch image

--- Quick search/ViewController.swift
+++ Quick search/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+  
+  let activityIndicator = UIActivityIndicatorView(style: .large)
+  @IBOutlet weak var searchWebView: WKWebView!
+  @IBOutlet weak var searchTextField: UITextField!
+  
+  private func setActivityIndicator() {
+    activityIndicator.center = view.center
+    activityIndicator.hidesWhenStopped = true
+    view.addSubview(activityIndicator)
+  }
+  
+  private func open(text: String) {
+    let someText = "https://"
+    if let searchUrl = URL(string: "\(someText)\(text)") {
+      let request = URLRequest(url: searchUrl)
+      searchWebView.load(request)
+    }
+  }
+  
+  @IBAction func goBack(_ sender: Any) {
+    searchWebView.goBack()
+  }
+  
+  @IBAction func goForward(_ sender: Any) {
+    searchWebView.goForward()
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    searchTextField.placeholder = "https://"
+    searchTextField.delegate = self
   }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    open(text: "google.com")
+  }
+  
+}
 
-
+extension ViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    if let text = textField.text {
+      open(text: text)
+    }
+    searchTextField.resignFirstResponder()
+    return true
+  }
 }
 
