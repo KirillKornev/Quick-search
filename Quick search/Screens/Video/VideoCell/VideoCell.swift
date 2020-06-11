7884492a85c6cfdf02b115b6facde14c24e84c90
Kirill <kirill_kornew@mail.ru>
2020-06-11 19:18:00 +0300

    video cell

---
+++ Quick search/Screens/Video/VideoCell/VideoCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoCell.swift
+//  Quick search
+//
+//  Created by Кирилл on 6/11/20.
+//  Copyright © 2020 Kornev.com. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Quick search/Screens/Video/VideoViewController.swift
+++ Quick search/Screens/Video/VideoViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class VideoViewController: UIViewController {
-
+  
+  @IBOutlet weak var tableView: UITableView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
       view.backgroundColor = .yellow
-//      navigationController?.navigationBar.prefersLargeTitles = true
       self.title = "Videos"
+      tableView.dataSource = self
+      tableView.delegate = self
+      setupTableViewCell()
+      tableView.estimatedRowHeight = 400
+      tableView.rowHeight = UITableView.automaticDimension
     }
+  
+  func setupTableViewCell() {
+    let nibCell = UINib(nibName: String(describing: VideoCell.self), bundle: nil)
+    tableView.register(nibCell, forCellReuseIdentifier: String(describing: VideoCell.self))
+  }
 
+}
+
+extension VideoViewController: UITableViewDataSource, UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: VideoCell.self), for: indexPath) as? VideoCell else { return UITableViewCell()}
+    //code
+    cell.contentView.backgroundColor = .green
+    return cell
+  }
+  
+  
 }
